480c6a25009d726489e705b92817eac3d96d18e8
Daniel Avila <roicesdaniel@gmail.com>
2022-09-25 22:32:55 -0400

    siguiendo el tutorial de midudev

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '@routes/App';
+import React from 'react'
+// eslint-disable-next-line no-unused-vars
+import ReactDOM from 'react-dom'
+import App from '@routes/App'
 
-
-
-import { createRoot } from 'react-dom/client';
-const container = document.getElementById('app');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App tab />);
-
-
+import { createRoot } from 'react-dom/client'
+const container = document.getElementById('app')
+const root = createRoot(container) // createRoot(container!) if you use TypeScript
+root.render(<App tab />)
 
 // ReactDOM.render(<App />, document.getElementById('app'));
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,51 +1,48 @@
-import Layout from '@containers/Layout';
-import Home from '@pages/Home';
-import Login from '@pages/Login';
-import PasswordRecovery from '@pages/PasswordRecovery';
-import SendEmail from '@pages/SendEmail';
-import NewPassword from '@pages/NewPassword';
-import MyAccount from '@pages/MyAccount';
-import CreateAccount from '@pages/CreateAccount';
-import Checkout from '@pages/Checkout';
-import Orders from '@pages/Orders';
+import Layout from '@containers/Layout'
+import Home from '@pages/Home'
+import Login from '@pages/Login'
+import PasswordRecovery from '@pages/PasswordRecovery'
+import SendEmail from '@pages/SendEmail'
+import NewPassword from '@pages/NewPassword'
+import MyAccount from '@pages/MyAccount'
+import CreateAccount from '@pages/CreateAccount'
+import Checkout from '@pages/Checkout'
+import Orders from '@pages/Orders'
 
-import NotFound from '@pages/NotFound';
+import NotFound from '@pages/NotFound'
 import '@styles/global.css'
-import AppContext from '@context/AppContext.js';
-import React from 'react';  //importamos el dom de react
-import { BrowserRouter, Router, Route, Switch } from 'react-router-dom';
+import AppContext from '@context/AppContext.js'
+import React from 'react' // importamos el dom de react
+// eslint-disable-next-line no-unused-vars
+import { BrowserRouter, Routes, Route, Switch } from 'react-router-dom'
 import useInitialState from '@hooks/useInitialState'
 
+const App = () => { // creamos el element
+  const initialState = useInitialState()
+  return (
+    <AppContext.Provider value={initialState}>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route exact path='/password-recovery' element={<PasswordRecovery />} />
+            <Route path='/send-email' element={<SendEmail />} />
+            <Route path='/new-password' element={<NewPassword />} />
+            <Route path='/account' element={<MyAccount />} />
+            <Route path='/signup' element={<CreateAccount />} />
+            <Route path='/checkout' element={<Checkout />} />
+            <Route path='/orders' element={<Orders />} />
+            <Route path='*' element={<NotFound />} />
 
-const App = () => {  //creamos el element
-    const initialState =  useInitialState();   
-    return (
-        <div>
-        <AppContext.Provider value={initialState}>
-        <BrowserRouter >
-            <Layout>
-                <Switch>
-                    <Route path="/" ><Home/></Route>
-                    <Route path="/login" ><Login/></Route>			
-                    <Route exact path="/password-recovery" element={<PasswordRecovery />} />
-                    <Route path="/send-email" element={<SendEmail />} />
-                    <Route path="/new-password" element={<NewPassword />} />
-                    <Route path="/account" element={<MyAccount/>} />
-                    <Route path="/signup" element={<CreateAccount/>} />
-                    <Route path="/checkout" element={<Checkout/>} />
-                    <Route path="/orders" element={<Orders/>} />
-                    <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </AppContext.Provider>
+  )
+}
 
-                </Switch>
-            </Layout>
-        </BrowserRouter> 
-        </AppContext.Provider>
-        </div>
-    );
-};
-    
-export default App;         //exportamos
-
+export default App // exportamos
 
 /* <Layout>
 {/* login se cierra ya que no va a contener otro componete, como si lo hace layout */
